1d8a4b226e80680f36dc83cd6cc47fb4a259f21c
SiddhanthGH <siddhanthghosh55@gmail.com>
2024-01-03 16:26:01 +0530

    Changed prepared stick texture and name to torch handle. User now needs to right-click on gravel or any wood to convert handle to temporary torch.

--- src/main/java/net/sydgh/emergencytorch/item/custom/TempTorchItem.java
+++ src/main/java/net/sydgh/emergencytorch/item/custom/TempTorchItem.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
@@ -39,6 +40,14 @@ public class TempTorchItem extends Item {
     }
 
     private boolean isValid(BlockState state) {
-        return state.isOf(Blocks.COBBLESTONE) || state.isOf(Blocks.ANDESITE);
+        return state.isOf(Blocks.ACACIA_LOG) ||
+                state.isOf(Blocks.BIRCH_LOG) ||
+                state.isOf(Blocks.CHERRY_LOG) ||
+                state.isOf(Blocks.JUNGLE_LOG) ||
+                state.isOf(Blocks.MANGROVE_LOG) ||
+                state.isOf(Blocks.OAK_LOG) ||
+                state.isOf(Blocks.SPRUCE_LOG) ||
+                state.isOf(Blocks.DARK_OAK_LOG) ||
+                state.isOf(Blocks.GRAVEL);
     }
 }
