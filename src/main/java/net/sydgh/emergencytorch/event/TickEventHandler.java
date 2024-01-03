f8c42b7c5f3036e3d13b4c92643a29ec0416b4f0
SiddhanthGH <siddhanthghosh55@gmail.com>
2024-01-03 15:30:02 +0530

    Removed all bugs, mod fully functional.

--- src/main/java/net/sydgh/emergencytorch/event/TickEventHandler.java
+++ src/main/java/net/sydgh/emergencytorch/event/TickEventHandler.java
@@ -10,46 +10,52 @@ import net.sydgh.emergencytorch.item.custom.TempTorchBlock;
 import java.util.ArrayList;
 
 public class TickEventHandler implements ServerTickEvents.StartWorldTick{
-    private static int i = 0;
     private static long tick;
     private static long diff;
     private static ArrayList<BlockPos> Pos = null;
     private static int Index;
     private static World World;
-    private static int tickTar = 600;
+    private static boolean timer1;
 
     public void breaker(){
             World.breakBlock(Pos.get(0), false);
             if (Index == 1){
-                EmergencyTorch.LOGGER.info("in if 1: "+Index);
-                Pos.remove(0);
                 TempTorchBlock.PlaceCount = 0;
+                timer1 = false;
+                Pos = null;
+                iStore = null;
             }
             else if(Index > 1){
-                i = 0;
                 Pos.remove(0);
+                iStore.remove(0);
                 Index = Pos.size();
-                EmergencyTorch.LOGGER.info("in if 2: "+Index);
             }
     }
+
+    private static ArrayList<Long> iStore;
 
     public static void getState(ArrayList<BlockPos> pos, World world){
         Pos = pos;
         World = world;
         Index = Pos.size();
-        EmergencyTorch.LOGGER.info("size: "+Index);
+        timer1 = true;
         if (Index == 1){
-        i = 0;}
+            iStore = new ArrayList<>();
+            iStore.add(tick);
+        }
+        else if (Index > 1){
+            iStore.add(tick);
+        }
     }
 
     @Override
     public void onStartTick(ServerWorld world) {
         tick = world.getTime();
-        long initial = (tick - i);
-        diff = tick - initial;
-        if(Pos != null && diff == tickTar){
-            breaker();
+        if (timer1) {
+            diff = tick - iStore.get(0);
+            if (Pos != null && diff == 1200) {
+                breaker();
+            }
         }
-        i++;
     }
 }
